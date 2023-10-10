2d51830fd12035421f0ebd5c372a5440a278a63f
Joab Urbano <joab.uaraujo@gmail.com>
2023-10-10 17:57:58 -0300

    Inicio da NavBar

--- duck-rpg-web/src/components/layout/NavBar.js
+++ duck-rpg-web/src/components/layout/NavBar.js
@@ -1,7 +1,14 @@
+import Styles from './NavBar.module.css';
+
 function NavBar() {
     return(
-        <div>
-            NavBar
+        <div className={Styles.navbar}>
+            <ui>
+                <li>Home</li>
+                <li>Buscar Magias</li>
+                <li>Sistema de XP</li>
+                <li>Sobre</li>
+            </ui>
         </div>
     )
 }

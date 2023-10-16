37d78ce80ffbdb8cfd695a5615f9dfa5972f14d6
Joab Urbano <joab.uaraujo@gmail.com>
2023-10-16 10:34:36 -0300

    Conclusão do primeiro modelo de busca de magias

--- duck-rpg-web/src/components/ConsultsDedApi/ConsultarTodasMagias.js
+++ duck-rpg-web/src/components/ConsultsDedApi/ConsultarTodasMagias.js
@@ -1,23 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../forms/Input.js';
+import Button from '../forms/Button.js'
 
 function ConsultarTodasMagias() {
 
+    const navigate = useNavigate();
+    const substituteCharacter = "-";
+
     const [spells, setSpells] = useState(null);
+
+    const [name, setName] = useState("");
+    const [data, setData] = useState(null);
 
     useEffect(() => {
         fetch(`https://www.dnd5eapi.co/api/spells`)
           .then(response => response.json())
           .then(spells => setSpells(spells))
           .catch(error => console.log(error))
-    }, [])
+    }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newName = name.split(' ').join(substituteCharacter);
+        fetch(`https://www.dnd5eapi.co/api/spells/${newName}`)
+          .then(response => response.json())
+          .then(data => {
+            setData(data);
+            if(data.index !== undefined) {
+                navigate(`/buscarmagias/magia/${data.index}`);
+            }
+            else {
+                const divWarning = document.getElementById("warning");
+                divWarning.innerHTML = "Magia não encontrada!";
+            }
+          })
+          .then(error => console.error(error));
+    }
 
     return(
         <div className='min-h-screen bg-cor-bg p-4'>
+            <div className='pb-8'>
+                    <form onSubmit={handleSubmit}>
+                        <div className='flex'>
+                            <Input type="text"
+                            text="Buscar magia"
+                            name="buscarMagia"
+                            placeholder="Digite o nome da magia"
+                            handleOnChange={(e) => setName(e.target.value)}
+                            value={name}
+                            />
+                            <div className='pl-4 text-xl text-red-800' id="warning"></div>
+                        </div>
+
+                        {name && (
+                            <Button type="submit" text="Consult" />
+                        )}
+                    </form>
+            </div>
             <ul className=''>
                 {spells && spells.results.map((item) => (
                             <li className='text-lg hover:text-orange-400 border-b-2 pb-1 border-red-200' key={item.name}>
                                 <Link to ={`/buscarmagias/magia/${item.index}`} >{item.name}</Link>
+
                             </li>
                         ))}
             </ul>
--- duck-rpg-web/src/components/forms/Button.js
+++ duck-rpg-web/src/components/forms/Button.js
@@ -1,6 +1,6 @@
 function Button({type, text}) {
     return(
-        <div className="text-gray-200 hover:text-orange-400 bg-cor-barra hover:bg-cor-barra-clara h-10 w-24 rounded-xl text-xl">
+        <div className="text-gray-200 text-xl text-center hover:text-orange-400 bg-cor-barra hover:bg-cor-barra-clara h-10 w-24 rounded-xl">
             <button type={type}>{text}</button>
         </div>
     )

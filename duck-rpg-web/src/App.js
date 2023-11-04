c6f9fcde6cecfb2cf1d59e9cd0d791b28a2baccf
Joab Urbano <joab.uaraujo@gmail.com>
2023-11-04 10:31:02 -0300

    feature(ConsultarTabelaClasses): Responsividade da tabela de classe

--- duck-rpg-web/src/App.js
+++ duck-rpg-web/src/App.js
@@ -1,38 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/layout/NavBar.js';
-import Footer from './components/layout/Footer.js';
-import Home from './components/pages/Home.js'
-import Sobre from './components/pages/Sobre.js'
-import BuscarMagias from './components/pages/BuscarMagias/BuscarMagias.js';
-import ConsultarMagia from './components/pages/BuscarMagias/ConsultarMagia.js';
-import BuscarClasses from './components/pages/BuscarClasses/BuscarClasses';
-import ConsultarClasses from './components/pages/BuscarClasses/ConsultarClasses';
-import SistemaXp from './components/pages/SistemaXp.js';
-import Registro from './components/pages/Registro.js';
-import Login from './components/pages/Login.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/layout/NavBar.js";
+import Footer from "./components/layout/Footer.js";
+import Home from "./components/pages/Home.js";
+import Sobre from "./components/pages/Sobre.js";
+import BuscarMagias from "./components/pages/BuscarMagias/BuscarMagias.js";
+import ConsultarMagia from "./components/pages/BuscarMagias/ConsultarMagia.js";
+import BuscarClasses from "./components/pages/BuscarClasses/BuscarClasses";
+import ConsultarClasses from "./components/pages/BuscarClasses/ConsultarClasses";
+import SistemaXp from "./components/pages/SistemaXp.js";
+import Registro from "./components/pages/Registro.js";
+import Login from "./components/pages/Login.js";
 
 function App() {
   return (
     <Router>
-      
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/buscarmagias" element={<BuscarMagias />} />
-        <Route path="/buscarmagias/magia/:spellSelected" element={<ConsultarMagia />} />
-
+        <Route
+          path="/buscarmagias/magia/:spellSelected"
+          element={<ConsultarMagia />}
+        />
         <Route path="/buscarclasses" element={<BuscarClasses />} />
-        <Route path="buscarclasse/:classSelected" element={<ConsultarClasses />} />
-        
+        <Route
+          path="buscarclasse/:classSelected"
+          element={<ConsultarClasses />}
+        />
         <Route path="/sistemaxp" element={<SistemaXp />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path ="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        min-h-screen min-w-full
       </Routes>
 
       <Footer />
-
     </Router>
   );
 }
--- duck-rpg-web/src/components/pages/BuscarClasses/ConsultarTabelaClasses.js
+++ duck-rpg-web/src/components/pages/BuscarClasses/ConsultarTabelaClasses.js
@@ -1,190 +1,188 @@
 import { useEffect, useState } from "react";
+import Loading from "../../layout/Loading";
 
-export default function ConsultarTabelaClasses({classSelected}) {
+export default function ConsultarTabelaClasses({ classSelected }) {
+  const [featuresClass, setFeaturesClass] = useState([0]);
+  const [removeLoading, setRemoveLoading] = useState(false);
+  const featuresClassList = [];
 
-    const [ featuresClass, setFeaturesClass ] = useState([0]);
-    const [ removeLoading, setRemoveLoading ] = useState(false);
+  useEffect(() => {
+    fetch(`https://www.dnd5eapi.co/api/classes/${classSelected}/levels`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeaturesClass(data);
+        setRemoveLoading(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setRemoveLoading(true);
+      });
+  }, [classSelected]);
 
-    useEffect(() => {
-        fetch(`https://www.dnd5eapi.co/api/classes/${classSelected}/levels`)
-        .then(response => response.json())
-        .then(data => {
-            setFeaturesClass(data);
-            setRemoveLoading(true);
-        })
-        .catch(error => {
-            console.log(error);
-            setRemoveLoading(true);
-        });
-    }, [classSelected]);
+  return (
+    <div className="whitespace-normal overflow-x-auto min-w-full">
+      <ul>
+        <li className="flex text-center font-bold gap-4">
+          <div className="w-32 min-w-[8em]">Level</div>
 
-    return(
-        <div>
-            <ul>
-                <li className='flex text-center font-bold'>
-                    <div className='w-1/12 bg-orange-400'>
-                        Level
-                    </div>
+          <div className="w-32 min-w-[8em]">Proficiency Bonus</div>
 
-                    <div className='w-1/12 bg-orange-400'>
-                        Proficiency Bonus
-                    </div>
+          <div className="w-96 min-w-[24em]">Features</div>
 
-                    <div className='w-3/12 bg-orange-400'>
-                        Features
-                    </div>
+          {featuresClass[0] && featuresClass[0].spellcasting ? (
+            <>
+              {featuresClass[0].spellcasting.cantrips_known && (
+                <div className="w-32 min-w-[8em]">Cantrips Known</div>
+              )}
 
-                    {featuresClass[0] && featuresClass[0].spellcasting ? (
-                            <>
-                                {featuresClass[0].spellcasting.cantrips_known && (
-                                    <div className="w-1/12 bg-orange-400">
-                                        Cantrips Known
-                                    </div>
-                                )}
+              {featuresClass[0].spellcasting.spells_known && (
+                <div className="w-32 min-w-[8em]">Spells Known</div>
+              )}
 
-                                {featuresClass[0].spellcasting.spells_known && (
-                                    <div className="w-1/12 bg-orange-400">
-                                        Spells Known
-                                    </div>
-                                )}
-                    
-                                {featuresClass[15].spellcasting.spell_slots_level_1 > 0 && (
-                                    <div className="w-4/12 bg-orange-400">
-                                        Slots<br/>
-                                        <div className='flex gap-5 items-center justify-center'>
-                                            <p>1st</p>
-                                            <p>2st</p>
-                                            <p>3st</p>
-                                            <p>4st</p>
-                                            <p>5st</p>
-                                            {featuresClass[15].spellcasting.spell_slots_level_6 > 0 && (
-                                                <>
-                                                    <p>6st</p>
-                                                    <p>7st</p>
-                                                    <p>8st</p>
-                                                    <p>9st</p>
-                                                </>
-                                            )}
-                                        </div>
-                                    </div>
-                                )}
+              {featuresClass[15].spellcasting.spell_slots_level_1 > 0 && (
+                <div className="w-96 min-w-[24em]">
+                  Slots
+                  <br />
+                  <div className="flex gap-4 items-center justify-center">
+                    <p>1st</p>
+                    <p>2st</p>
+                    <p>3st</p>
+                    <p>4st</p>
+                    <p>5st</p>
+                    {featuresClass[15].spellcasting.spell_slots_level_6 > 0 && (
+                      <>
+                        <p>6st</p>
+                        <p>7st</p>
+                        <p>8st</p>
+                        <p>9st</p>
+                      </>
+                    )}
+                  </div>
+                </div>
+              )}
 
-                                {featuresClass[0].spellcasting.spell_slots_level_1 > 0 && featuresClass[15].spellcasting.spell_slots_level_1 === 0 && (
-                                    <div className="w-1/12 bg-orange-400">
-                                        Slot Level<br/>
-                                    </div>
-                                )}
-                            </>
-                        ) : (
-                            <></>
-                        )}
-                </li>
+              {featuresClass[0].spellcasting.spell_slots_level_1 > 0 &&
+                featuresClass[15].spellcasting.spell_slots_level_1 === 0 && (
+                  <div className="w-32 min-w-[8em]">
+                    Slot Level
+                    <br />
+                  </div>
+                )}
+            </>
+          ) : (
+            <></>
+          )}
+        </li>
 
+        {featuresClass &&
+          featuresClass.map((features) => {
+            return (
+              <li className="flex text-center gap-4">
+                <div className="w-32 min-w-[8em]">
+                  {features.level}
+                  <br />
+                </div>
 
-                
-                {featuresClass && featuresClass.map((features) => {
-                    return(
-                        <li className='flex text-center'>
-                            <div className='w-1/12 bg-slate-300'>
-                                {features.level}<br/>
-                            </div>
+                <div className="w-32 min-w-[8em]">+{features.prof_bonus}</div>
 
-                            <div className='w-1/12 bg-slate-300'>
-                                +{features.prof_bonus}
-                            </div>
-
-                            <div className='w-3/12 bg-slate-300'>
-                                {features && features.features.map((feature, index) => (
-                                <p className = "pr-1">
-                                    {index === features.features.length - 1 ? (
-                                    <span key={index}>{feature.name}</span>
-                                    ) : (
-                                    <span key={index}>{feature.name}, </span>
-                                    )}
-                                </p>
-                                ))}
-                            </div>
-                    
-                            {features.spellcasting && (
-                                <>
-                                    {featuresClass[0].spellcasting.cantrips_known && (
-                                        <div className='w-1/12 bg-slate-300'>
-                                            {features.spellcasting.cantrips_known}
-                                        </div>
-                                    )}
-
-                                    {featuresClass[0].spellcasting.spells_known && (
-                                        <div className='w-1/12 bg-slate-300'>
-                                            {features.spellcasting.spells_known}
-                                        </div>
-                                    )}
-
-                                    {featuresClass[15].spellcasting.spell_slots_level_1 > 0 && (
-                                        <div className="w-4/12 flex gap-8 items-center justify-center bg-slate-300">
-                                            <p>
-                                                {features.spellcasting.spell_slots_level_1}
-                                            </p>
-                                            <p>
-                                                {features.spellcasting.spell_slots_level_2}
-                                            </p>
-                                            <p>
-                                                {features.spellcasting.spell_slots_level_3}
-                                            </p>
-                                            <p>
-                                                {features.spellcasting.spell_slots_level_4}
-                                            </p>
-                                            <p>
-                                                {features.spellcasting.spell_slots_level_5}
-                                            </p>
-
-                                            {featuresClass[15].spellcasting.spell_slots_level_6 > 0 && (
-                                                <>
-                                                    <p>
-                                                        {features.spellcasting.spell_slots_level_6}
-                                                    </p>
-                                                    <p>
-                                                        {features.spellcasting.spell_slots_level_7}
-                                                    </p>
-                                                    <p>
-                                                        {features.spellcasting.spell_slots_level_8}
-                                                    </p>
-                                                    <p>
-                                                        {features.spellcasting.spell_slots_level_9}
-                                                    </p>
-                                                </>
-                                            )}
-                                        </div>
-                                    )}
-
-                                    {featuresClass[0].spellcasting.spell_slots_level_1 > 0 && featuresClass[15].spellcasting.spell_slots_level_1 === 0 && (
-                                        <div className="w-1/12 flex items-center justify-center bg-slate-300">
-                                            <p>{features.spellcasting.spell_slots_level_1 > 0 && (
-                                                <>1st - {features.spellcasting.spell_slots_level_1} slots</>
-                                            )}</p>
-
-                                            <p>{features.spellcasting.spell_slots_level_2 > 0 && (
-                                                <>2st - {features.spellcasting.spell_slots_level_2} slots</>
-                                            )}</p>
-
-                                            <p>{features.spellcasting.spell_slots_level_3 > 0 && (
-                                                <>3st - {features.spellcasting.spell_slots_level_3} slots</>
-                                            )}</p>
-
-                                            <p>{features.spellcasting.spell_slots_level_4 > 0 && (
-                                                <>4st - {features.spellcasting.spell_slots_level_4} slots</>
-                                            )}</p>
-
-                                            <p>{features.spellcasting.spell_slots_level_5 > 0 && (
-                                                <>5st - {features.spellcasting.spell_slots_level_5} slots</>
-                                            )}</p>
-                                        </div>
-                                    )}
-                                </>
-                            )}
-                        </li>
+                <div className="w-96 min-w-[24em]">
+                  {features &&
+                    features.features.map((feature, index) => {
+                    featuresClassList.push(feature.name)
+                      return(
+                      <p className="pr-1">
+                      {index === features.features.length - 1 ? (
+                        <span key={index}>{feature.name}</span>
+                      ) : (
+                        <span key={index}>{feature.name}, </span>
+                      )}
+                    </p>
                     )
-                })}
-            </ul>
-        </div>
-    )
+          })}
+                </div>
+
+                {features.spellcasting && (
+                  <>
+                    {featuresClass[0].spellcasting.cantrips_known && (
+                      <div className="w-32 min-w-[8em]">
+                        {features.spellcasting.cantrips_known}
+                      </div>
+                    )}
+
+                    {featuresClass[0].spellcasting.spells_known && (
+                      <div className="w-32 min-w-[8em]">
+                        {features.spellcasting.spells_known}
+                      </div>
+                    )}
+
+                    {featuresClass[15].spellcasting.spell_slots_level_1 > 0 && (
+                      <div className="w-96 min-w-[24em] flex gap-7 items-center justify-center">
+                        <p>{features.spellcasting.spell_slots_level_1}</p>
+                        <p>{features.spellcasting.spell_slots_level_2}</p>
+                        <p>{features.spellcasting.spell_slots_level_3}</p>
+                        <p>{features.spellcasting.spell_slots_level_4}</p>
+                        <p>{features.spellcasting.spell_slots_level_5}</p>
+
+                        {featuresClass[15].spellcasting.spell_slots_level_6 >
+                          0 && (
+                          <>
+                            <p>{features.spellcasting.spell_slots_level_6}</p>
+                            <p>{features.spellcasting.spell_slots_level_7}</p>
+                            <p>{features.spellcasting.spell_slots_level_8}</p>
+                            <p>{features.spellcasting.spell_slots_level_9}</p>
+                          </>
+                        )}
+                      </div>
+                    )}
+
+                    {featuresClass[0].spellcasting.spell_slots_level_1 > 0 &&
+                      featuresClass[15].spellcasting.spell_slots_level_1 ===
+                        0 && (
+                        <div className="w-32 min-w-[8em] flex items-center justify-center">
+                          <p>
+                            {features.spellcasting.spell_slots_level_1 > 0 && (
+                              <>
+                                1st -{" "}{features.spellcasting.spell_slots_level_1}{" "}slots
+                              </>
+                            )}
+                          </p>
+                          <p>
+                            {features.spellcasting.spell_slots_level_2 > 0 && (
+                              <>
+                                2st -{" "}{features.spellcasting.spell_slots_level_2}{" "}slots
+                              </>
+                            )}
+                          </p>
+                          <p>
+                            {features.spellcasting.spell_slots_level_3 > 0 && (
+                              <>
+                                3st -{" "}{features.spellcasting.spell_slots_level_3}{" "}slots
+                              </>
+                            )}
+                          </p>
+                          <p>
+                            {features.spellcasting.spell_slots_level_4 > 0 && (
+                              <>
+                                4st -{" "}{features.spellcasting.spell_slots_level_4}{" "}slots
+                              </>
+                            )}
+                          </p>
+                          <p>
+                            {features.spellcasting.spell_slots_level_5 > 0 && (
+                              <>
+                                5st -{" "}{features.spellcasting.spell_slots_level_5}{" "}slots
+                              </>
+                            )}
+                          </p>
+                        </div>
+                      )}
+                  </>
+                )}
+              </li>
+            );
+          })}
+      </ul>
+      {!removeLoading && <Loading />}
+    </div>
+  );
 }

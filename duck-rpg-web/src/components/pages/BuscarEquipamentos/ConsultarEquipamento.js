6ebbbccbc07c8b71faa6e585fbc851911a0c43cb
Joab Urbano <joab.uaraujo@gmail.com>
2023-12-26 17:43:20 -0300

    Renderização condicional dos atributos dos equipamentos

--- duck-rpg-web/src/components/pages/BuscarEquipamentos/ConsultarEquipamento.js
+++ duck-rpg-web/src/components/pages/BuscarEquipamentos/ConsultarEquipamento.js
@@ -22,8 +22,8 @@ export default function ConsultarEquipamento() {
         <div className='min-h-screen bg-cor-bg p-4'>
             {equipament && (
                 <ul>
-                    <li className='text-orange-800 text-4xl pl-1'>{equipament.name}</li>
-                    <li className='text-2xl'>{equipament.equipment_category.name}</li>
+                    <li className='text-orange-800 text-4xl pl-1 pb-2'>{equipament.name}</li>
+                    <li className='text-2xl pb-1'>{equipament.equipment_category.name}</li>
 
                     <li className='text-lg'>
                     {equipament.gear_category && equipament.gear_category.name ? (
@@ -37,12 +37,68 @@ export default function ConsultarEquipamento() {
                     {equipament.tool_category && equipament.tool_category}
                     </li>
 
-                    <li className='text-lg'>{equipament.weight && (
+                    <li className='text-lg'>{equipament.weight && equipament.weight > 0 ? (
                         <>Weight: {equipament.weight} pounds</> 
+                        ):(<></>)}
+                    </li>
+
+                    <li className='text-lg'>{equipament.armor_class && (
+                        <>
+                            Armor Class: {equipament.armor_class.base} {equipament.armor_class.dex_bonus === true ? (<> + dex max {equipament.armor_class.max_bonus}</>) : (<></>)}
+                        </> 
                         )}
                     </li>
 
-                    <li className='text-lg'>Cost: {equipament.cost.quantity} {equipament.cost.unit}</li>
+                    <li className='text-lg'>{equipament.str_minimum ? (
+                        <>Minimum force required: {equipament.str_minimum}</>
+                        ):(<></>)}
+                    </li>
+
+                    <li className='text-lg'>{equipament.stealth_disadvantage ? (
+                        <>Disadvantage on stealth checks</>
+                        ):(<></>)}
+                    </li>
+
+                    <li className='text-lg'>{equipament.weapon_category ? (
+                        <>Weapon category: {equipament.weapon_category}</>
+                        ):(<></>)}
+                    </li>
+
+                    <li className='text-lg'>{equipament.damage ? (
+                        <>Damage {equipament.damage.damage_dice} {equipament.damage.damage_type.name}</>
+                        ):(<></>)}
+                    </li>
+
+                    <li className='text-lg'>{equipament.weapon_range ? (
+                        <>Weapon range: {equipament.weapon_range}</>
+                        ):(<></>)}
+                    </li>
+
+                    <li className='text-lg'>{equipament.properties && equipament.properties.length > 0 ? (
+                        <>
+                            <p className='font-bold'>Properties: </p>
+                            <ul>
+                                <li>
+                                    {equipament.properties.map((propertie, index) => {
+                                        return(
+                                            <>
+                                                {index === equipament.properties.length - 1 ? (
+                                                    <span key={index}>{propertie.name}.</span>
+                                                ) : (
+                                                    <span key={index}>{propertie.name}, </span>
+                                                )}
+                                            </>
+                                        )
+                                    })}
+                                </li>
+                            </ul>
+                        </>
+                    ):(<></>)}</li>
+
+                    <li className='text-lg'>{equipament.cost.quantity > 0 &&(
+                        <>Cost: {equipament.cost.quantity} {equipament.cost.unit}</>
+                    )}
+                    </li>
                     <li className='text-lg'>{equipament.propities && (
                         <>Propities: {equipament.propities}</> 
                         )}

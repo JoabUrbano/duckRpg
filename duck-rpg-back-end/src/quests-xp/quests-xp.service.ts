57b83887b885386fc77364365bb70da5595e43f3
Joab Urbano <joab.uaraujo@gmail.com>
2024-01-20 10:43:39 -0300

    Criação dos metodos no quest-xp.service

--- duck-rpg-back-end/src/quests-xp/quests-xp.service.ts
+++ duck-rpg-back-end/src/quests-xp/quests-xp.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { QuestXp } from './entities/quest-xp.entity';
+import { CreateQuestXpDto } from './dto/create-quest-xp.dto';
+import { UpdateQuestXpDto } from './dto/update-quest-xp.dto';
 
 @Injectable()
-export class QuestsXpService {}
+export class QuestsXpService {
+  constructor(
+    @InjectRepository(QuestXp)
+    private readonly questXpRepository: Repository<QuestXp>,
+  ) {}
+
+  async findAll(): Promise<QuestXp[]> {
+    return 1;
+  }
+
+  async findOneById(id: number) {
+    return 1;
+  }
+
+  async create(createQuestXpDto: CreateQuestXpDto) {
+    return 1;
+  }
+
+  async update(updateQuestXpDto: UpdateQuestXpDto) {
+    return 1;
+  }
+
+  async remove(id: number) {
+    return 1;
+  }
+}

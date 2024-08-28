082dfbf00894fbb1b2f696645b1f6b3677f48d60
JoabUrbano <joab.uaraujo@gmail.com>
2024-08-28 12:13:22 -0300

    Refactore(PageSpell): Geração de botões de filtro da consulta dinamicamente

--- duck-rpg-web/src/components/pages/SearchSpells/PageSpell.js
+++ duck-rpg-web/src/components/pages/SearchSpells/PageSpell.js
@@ -9,19 +9,9 @@ export default function PageSpell() {
 
     const navigate = useNavigate();
 
-    const [colorButtonTextVar00, setColorButtonTextVar00] = useState("text-gray-200");
-    const [colorButtonTextVar01, setColorButtonTextVar01] = useState("text-gray-200");
-    const [colorButtonTextVar02, setColorButtonTextVar02] = useState("text-gray-200");
-    const [colorButtonTextVar03, setColorButtonTextVar03] = useState("text-gray-200");
-    const [colorButtonTextVar04, setColorButtonTextVar04] = useState("text-gray-200");
-    const [colorButtonTextVar05, setColorButtonTextVar05] = useState("text-gray-200");
-    const [colorButtonTextVar06, setColorButtonTextVar06] = useState("text-gray-200");
-    const [colorButtonTextVar07, setColorButtonTextVar07] = useState("text-gray-200");
-    const [colorButtonTextVar08, setColorButtonTextVar08] = useState("text-gray-200");
-    const [colorButtonTextVar09, setColorButtonTextVar09] = useState("text-gray-200");
-
     const [name, setName] = useState("");
     const [filter, setFilter] = useState("");
+    const [buttonColors, setButtonColors] = useState(Array(10).fill("text-gray-200"));
 
     const {dataDed, removeLoading, error} = UseFetchApiDed(
         `https://www.dnd5eapi.co/api/spells${filter}`,
@@ -50,21 +40,12 @@ export default function PageSpell() {
     }
 
     const handleFilterChange = (levelSpell) => {
-        setColorButtonTextVar00("text-gray-200");
-        setColorButtonTextVar01("text-gray-200");
-        setColorButtonTextVar02("text-gray-200");
-        setColorButtonTextVar03("text-gray-200");
-        setColorButtonTextVar04("text-gray-200");
-        setColorButtonTextVar05("text-gray-200");
-        setColorButtonTextVar06("text-gray-200");
-        setColorButtonTextVar07("text-gray-200");
-        setColorButtonTextVar08("text-gray-200");
-        setColorButtonTextVar09("text-gray-200");
-        if(`?level=${levelSpell}` !== filter)
-            setFilter(`?level=${levelSpell}`);
-        else{
-            setFilter("");
-        }
+        const updatedColors = buttonColors.map((color, index) => 
+            index === levelSpell ?color !== "text-orange-500" ? "text-orange-500" : "text-gray-200"  : "text-gray-200"
+        );
+        console.log(updatedColors);
+        setButtonColors(updatedColors);
+        setFilter(`?level=${levelSpell}`!== filter ? `?level=${levelSpell}` : "");
     }
 
     return(
@@ -88,97 +69,15 @@ export default function PageSpell() {
                     </form>
             </div>
 
-            <div className='flex items-center justify-center'>
-                <Button text="Level 0" onClick={() => {
-                    handleFilterChange(0);
-                    setColorButtonTextVar00(
-                    colorButtonTextVar00 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level00" colorButtonText = {colorButtonTextVar00}
-                />
-                <Button text="Level 1" onClick={() => {
-                    handleFilterChange(1);
-                    setColorButtonTextVar01(
-                    colorButtonTextVar01 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level01" colorButtonText = {colorButtonTextVar01}
-                />
-                <Button text="Level 2" onClick={() => {
-                    handleFilterChange(2);
-                    setColorButtonTextVar02(
-                    colorButtonTextVar02 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level02" colorButtonText = {colorButtonTextVar02}
-                />
-                <Button text="Level 3" onClick={() => {
-                    handleFilterChange(3);
-                    setColorButtonTextVar03(
-                    colorButtonTextVar03 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level03" colorButtonText = {colorButtonTextVar03}
-                />
-                <Button text="Level 4" onClick={() => {
-                    handleFilterChange(4);
-                    setColorButtonTextVar04(
-                    colorButtonTextVar04 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level04" colorButtonText = {colorButtonTextVar04}
-                />
-                <Button text="Level 5" onClick={() => {
-                    handleFilterChange(5);
-                    setColorButtonTextVar05(
-                    colorButtonTextVar05 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level05" colorButtonText = {colorButtonTextVar05}
-                />
-                <Button text="Level 6" onClick={() => {
-                    handleFilterChange(6);
-                    setColorButtonTextVar06(
-                    colorButtonTextVar06 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level06" colorButtonText = {colorButtonTextVar06}
-                />
-                <Button text="Level 7" onClick={() => {
-                    handleFilterChange(7);
-                    setColorButtonTextVar07(
-                    colorButtonTextVar07 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level07" colorButtonText = {colorButtonTextVar07}
-                />
-                <Button text="Level 8" onClick={() => {
-                    handleFilterChange(8);
-                    setColorButtonTextVar08(
-                    colorButtonTextVar08 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level08" colorButtonText = {colorButtonTextVar08}
-                />
-                <Button text="Level 9" onClick={() => {
-                    handleFilterChange(9);
-                    setColorButtonTextVar09(
-                    colorButtonTextVar09 !== "text-orange-500"
-                        ? "text-orange-500"
-                        : "text-gray-200"
-                    );
-                }} id = "level09" colorButtonText = {colorButtonTextVar09}
-                />
+            <div className='flex items-center justify-center gap-1'>
+                {buttonColors.map((color, index) => (
+                    <Button 
+                        key = {index}
+                        text = {`Level ${index}`}
+                        onClick={() => handleFilterChange(index)}
+                        colorButtonText={color}
+                    />
+                ))}
             </div>
 
             <div className="text-2xl p-2 text-orange-700" id="levelSpell">{filter !== "" && (<>Spells level {filter.split("=")[1]}</>)}</div>
